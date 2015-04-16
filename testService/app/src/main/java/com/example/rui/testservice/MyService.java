cac7c0c37e25721ce0114f813e5674c78171899e
zhangrui5 <zhangrui5@letv.com>
2015-04-16 17:22:02 +0800

    make the service foreground by Notification.

--- testService/app/src/main/java/com/example/rui/testservice/MyService.java
+++ testService/app/src/main/java/com/example/rui/testservice/MyService.java
@@ -1,5 +1,7 @@
 package com.example.rui.testservice;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
@@ -21,6 +23,17 @@ public class MyService extends Service {
     public void onCreate() {
         super.onCreate();
         Log.d(TAG, "onCreate" + " @threadid=" + Thread.currentThread().getId());
+
+        Intent notiIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notiIntent, 0);
+        Notification noti = new Notification.Builder(this)
+                .setContentTitle("Title")
+                .setContentText("this is content")
+                .setSmallIcon(R.drawable.abc_ic_commit_search_api_mtrl_alpha)
+                .setContentIntent(pendingIntent)
+                .build();
+
+        startForeground(1, noti);
     }
 
     /**

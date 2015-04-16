38eeabbd269853c7fc403a6cacf41c6c7405f826
zhangrui5 <zhangrui5@letv.com>
2015-04-16 15:05:36 +0800

    add a simple service.
    1. extends Service and override;
    2. declare the service in manifest;
    3. start&stop service by intent in activity;

---
+++ testService/app/src/main/java/com/example/rui/testservice/MyService.java
@@ -0,0 +1,38 @@
+package com.example.rui.testservice;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MyService extends Service {
+
+    public static final String TAG = "MyService";
+    public MyService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "onCreate");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+//        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
+    }
+}
